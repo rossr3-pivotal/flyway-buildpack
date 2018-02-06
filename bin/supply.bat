38d8ccec1b75c71471d1c07665cea83ac3089be4
Rick Ross <rross@pivotal.io>
2018-02-06 17:43:56 -0500

    Added Windows support and changed Linux script

---
+++ bin/supply.bat
@@ -0,0 +1,2 @@
+@echo off
+powershell.exe -ExecutionPolicy Unrestricted %~dp0\supply.ps1 %1 %2 %3 %4
---
+++ bin/supply.ps1
@@ -0,0 +1,37 @@
+#
+# supply.ps1 for Flyway
+# 
+Param(
+  [Parameter(Mandatory=$True,Position=1)]
+    [string]$BuildDir,
+  [Parameter(Mandatory=$True,Position=2)]
+    [string]$CacheDir,
+  [Parameter(Mandatory=$True,Position=3)]
+    [string]$DepDir,
+  [Parameter(Mandatory=$True,Position=4)]
+    [string]$SubDir    
+)
+
+Write-Host "Supplying Flyway 5.0.7"
+
+$url = "https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/5.0.7/flyway-commandline-5.0.7-windows-x64.zip"
+$BinDir = $DepDir\$SubDir\bin
+
+New-Item -ItemType directory -Path $BinDir
+
+# download the file
+Write-Host "Downloading Flyway..."
+
+$output = $BinDir + "\flyway.zip"
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($url, $output)
+Write-Host "Flyway has been downloaded"
+
+Write-Host "Unzipping flyway"
+Expand-Archive $output -DestinationPath $BinDir
+Move-Item $BinDir\flyway-5.0.7\* $BinDir
+
+# clean up
+Write-Host "Cleaning up..."
+Remove-Item -path $BinDir\flyway-5.0.7 -recurse
+Remove-Item -path $BinDir\flyway.zip
